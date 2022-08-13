4dab2dc9c9bd04244d460e0f14caff152eb02206
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-08-13 22:49:15 +0530

    Add the LeetCode practice docs

---
+++ LeetCode Practice/script.js
@@ -0,0 +1,47 @@
+console.log("LeetCode Practice");
+
+//1.
+//https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
+//A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+// Return the maximum number of words that appear in a single sentence.
+
+let sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"];
+
+function calcLength() {
+    let max = 0;
+    for (let sentence of sentences) {
+        let newArray = sentence.split(" ");
+        let lengthOfElement = newArray.length;
+
+        if (max < lengthOfElement) {
+            max = lengthOfElement;
+        }
+
+        console.log(lengthOfElement);
+    }
+    return max;
+}
+
+// console.log(calcLength());
+
+
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+let nums = [8, 1, 2, 2, 3];
+
+function smallerNumbersThanCurrent() {
+    let countArray = [];
+    for (let i of nums) {
+        let count = 0;
+        for (let j of nums) {
+            if (i != j && j < i) {
+                count++;
+            }
+        }
+        countArray.push(count);
+    }
+    return countArray;
+}
+
+console.log(smallerNumbersThanCurrent());
