3d67ba377d7280c484c58a6388b3b574edad4d1a
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-08-16 21:39:05 +0530

    Add ques 10

--- LeetCode Practice/script.js
+++ LeetCode Practice/script.js
@@ -128,3 +128,27 @@ var containsDuplicate = function(nums) {
 }; 
 
 console.log(containsDuplicate([1,2,3,4]));
+
+
+// 10.
+// https://leetcode.com/problems/fizz-buzz/
+function printFizzBuzz(n) {
+    let answer = [];
+    for (let i = 1; i <= n; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            answer.push("FizzBuzz");
+        }
+        else if (i % 3 == 0) {
+            answer.push("Fizz");
+        }
+        else if (i % 5 == 0) {
+            answer.push("Buzz");
+        }
+        else {
+            answer.push(`${i}`);
+        }
+    }
+    return answer;
+}
+
+// console.log(printFizzBuzz(15));
