84137f6022018036093f0b4aa5f0641a0bbf15bd
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-08-17 21:33:13 +0530

    Add two sum problem

--- LeetCode Practice/script.js
+++ LeetCode Practice/script.js
@@ -197,3 +197,32 @@ function arrayStringsAreEqual() {
 }
 
 console.log(arrayStringsAreEqual());
+
+
+
+// 12.
+// https://leetcode.com/problems/two-sum/
+function twoSum(nums, target) {
+    let outputArray = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                if (i == j) {
+                    continue;
+                }
+                else {
+                    outputArray.push(i, j);
+                }
+                // outputArray.push(i, j);
+            }
+        }
+    }
+    outputArray.length = 2;
+    return outputArray;
+}
+
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
+
+// console.log(twoSum([2, 5, 5, 11], 10));
