041493300e481a36f5bc9a45016f45ac7fcbe2d6
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-08-26 14:17:12 +0530

    Add leetcode.com/problems/binary-search/

--- LeetCode Practice/script.js
+++ LeetCode Practice/script.js
@@ -365,3 +365,27 @@ function isAnagram(s, t) {
 
 console.log(isAnagram("rat", "car"));
 console.log(isAnagram("anagram", "nagaram"));
+
+
+// BINARY SEARCH 
+// https://leetcode.com/problems/binary-search/
+// (Ascending Order)
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2);
+        
+        if (target < nums[mid]) {
+            end = mid - 1;
+        }
+        else if (target > nums[mid]) {
+            start = mid + 1;
+        }
+        else {
+            return mid;
+        }
+    }
+    return -1;
+};
