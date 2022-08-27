44b3d2790650625fece884e8e29ed1cf212c4698
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-08-27 19:33:22 +0530

    Add find-smallest-letter-greater-than-target/

--- LeetCode Practice/script.js
+++ LeetCode Practice/script.js
@@ -389,3 +389,28 @@ var search = function(nums, target) {
     }
     return -1;
 };
+
+
+
+// 19.
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+function nextGreatestLetter(letters, target) {
+    // Apply binary search (non-decreasing order of array)
+
+    let start = 0;
+    let end = letters.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2);
+
+        if (target < letters[mid]) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return letters[start % letters.length];
+}
+
+// console.log(nextGreatestLetter(["c","f","j"], "a"));
